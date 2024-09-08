692f1362693431a9d99b87ab767ce39395ea3697
yang <yangvvva@gmail.com>
2024-09-08 15:44:08 -0400

    update comments

--- myzhxy/src/main/java/com/atguigu/myzhxy/controller/ClazzController.java
+++ myzhxy/src/main/java/com/atguigu/myzhxy/controller/ClazzController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Api(tags = "班级管理器")
+@Api(tags = "Class Controller")
 @RestController
 @RequestMapping("/sms/clazzController")
 public class ClazzController {
